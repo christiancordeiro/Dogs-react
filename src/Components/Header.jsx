911cb88a9e27393248c48d271df971201de17fb7
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-03-18 16:54:41 -0300

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react"
 import styles from "./Header.module.css"
 import { Link } from "react-router-dom"
+import LogoDogs from "../Assets/dogs.svg?react"
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <LogoDogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
