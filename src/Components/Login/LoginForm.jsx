3258c27e1ea3c12f0a679bb2b6100925f1d4bc0b
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-03-21 14:42:58 -0300

    userContext

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,41 +1,21 @@
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Input from "../Form/Input"
 import Button from "../Form/Button"
 import useForm from "../../Hooks/useForm"
-import { TOKEN_POST, USER_GET } from "../../api"
+import { UserContext } from "../../UserContext"
 
 const LoginForm = () => {
   const username = useForm()
   const password = useForm()
 
-  useEffect(() => {
-    const token = window.localStorage.getItem("token")
-    if (token) getUser(token)
-  }, [])
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token)
-
-    const response = await fetch(url, options)
-    const json = await response.json()
-    console.log(json)
-  }
+  const { userLogin } = useContext(UserContext)
 
   async function handleSubmit(e) {
     e.preventDefault()
 
-    const { url, options } = TOKEN_POST({
-      username: username.value,
-      password: password.value,
-    })
-
     if (username.validate() && password.validate()) {
-      const response = await fetch(url, options)
-      const json = await response.json()
-      console.log(json)
-      window.localStorage.setItem("token", json.token)
-      getUser(json.token)
+      userLogin(username.value, password.value)
     }
   }
 
